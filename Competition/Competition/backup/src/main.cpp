12924f03eb3a31063b196dad54e38a13fb7c6afc
LaytonRobotics <56170971+LaytonRobotics@users.noreply.github.com>
2019-11-20 16:27:51 -0700

    Add files via upload

--- Competition/Competition/backup/src/main.cpp
+++ Competition/Competition/backup/src/main.cpp
@@ -1,30 +1,3 @@
-// To complete the VEXcode V5 Text project upgrade process, please follow the
-// steps below.
-// 
-// 1. You can use the Robot Configuration window to recreate your V5 devices
-//   - including any motors, sensors, 3-wire devices, and controllers.
-// 
-// 2. All previous code located in main.cpp has now been commented out. You
-//   will need to migrate this code to the new "int main" structure created
-//   below and keep in mind any new device names you may have set from the
-//   Robot Configuration window. 
-// 
-// If you would like to go back to your original project, a complete backup
-// of your original (pre-upgraded) project was created in a backup folder
-// inside of this project's folder.
-
-// ---- START VEXCODE CONFIGURED DEVICES ----
-// ---- END VEXCODE CONFIGURED DEVICES ----
-
-#include "vex.h"
-
-using namespace vex;
-
-
-// ---- START VEXCODE CONFIGURED DEVICES ----
-// Robot Configuration:
-// [Name]               [Type]        [Port(s)]
-// ---- END VEXCODE CONFIGURED DEVICES ----
 #include "vex.h"
 #include "vision.h"
 using namespace vex;
@@ -150,7 +123,6 @@ void usercontrol( void ) {
 }
 
 int main() {
-  vexcodeInit();
     //Set up callbacks for autonomous and driver control periods.
     Competition.autonomous( autonomous );
     Competition.drivercontrol( usercontrol );
---
+++ Competition/Competition/src/robot-config.cpp
@@ -0,0 +1,22 @@
+#include "vex.h"
+
+using namespace vex;
+using signature = vision::signature;
+using code = vision::code;
+
+
+
+// VEXcode device constructors
+
+// VEXcode generated functions
+
+
+
+/**
+ * Used to initialize code/tasks/devices added using tools in VEXcode Text.
+ * 
+ * This should be called at the start of your int main function.
+ */
+void vexcodeInit( void ) {
+  // nothing to initialize
+}
