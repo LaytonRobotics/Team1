30cd63c34183f8ef640e8e4447f40a516b1cc27e
LaytonRobotics <56170971+LaytonRobotics@users.noreply.github.com>
2019-11-21 16:33:24 -0700

    Add files via upload

--- Competition/Competition/backup/src/main.cpp
+++ Competition/Competition/backup/src/main.cpp
@@ -12,8 +12,8 @@ motor BL = motor(PORT2, ratio18_1, false);
 motor FR = motor(PORT20, ratio18_1, true);
 motor BR = motor(PORT19, ratio18_1, true);
 motor A = motor(PORT13, ratio36_1, false);
-motor IL = motor(PORT15, ratio36_1, true);
-motor IR = motor(PORT12, ratio36_1, false);
+motor IL = motor(PORT4, ratio36_1, true);
+motor IR = motor(PORT16, ratio36_1, false);
 motor C = motor(PORT3, ratio36_1, true);
 motor_group LM(FL, BL);
 motor_group RM(FR, BR);
@@ -36,21 +36,71 @@ void driveX(double speed_pct, double Degrees){
 }
 
 void rotate(double d){
-  AutoDrive.turnFor(turnType::right, d*1.451, deg, 100, rpm);
+  AutoDrive.turnFor(turnType::right, d*1.2, deg, 100, rpm);
 }
-
 
 void pre_auton( void ) {
   C.resetRotation();
+  A.resetRotation();
+  C.setVelocity(20, pct);
+}
+
+void drive(double inches,double vel=100){
+  AutoDrive.setDriveVelocity(vel, velocityUnits::pct);
+  AutoDrive.driveFor(fwd, inches, distanceUnits::in);
+}
+
+void setIntakeSpeed(double speed){
+  IR.setVelocity(speed,pct);
+  IL.setVelocity(speed,pct);
+  IR.spin(fwd);
+  IL.spin(fwd);
+}
+
+void stopIntake(){
+  IR.stop(brake);
+  IL.stop(brake);
+}
+
+void armUp(){
+  A.setVelocity(100, pct);
+  A.rotateTo(650,deg);
+  A.rotateTo(0,deg);
+  A.rotateTo(90,deg);
+}
+
+
+void auto3(){
+  int dist = 50;
+  armUp();
+  setIntakeSpeed(90);
+  drive(12,80);
+  drive(dist-12,12);
+  //setIntakeSpeed(-50);
+  //task::sleep(1500);
+  stopIntake();
+  drive(-dist+5,100);
+  rotate(90);
+  drive(12,50);
+  rotate(30);
+  drive(5,60);
+  setIntakeSpeed(-20);
+  task::sleep(2250);
+  stopIntake();
+  A.rotateTo(-5,deg);
+  C.rotateTo(175,deg);
+  drive(2,15);
+  task::sleep(2000);
+  drive(-5,75);
+  C.rotateTo(0, deg);
 }
 
 void autonomous( void ) {
-  //AutoDrive.driveFor(fwd, 14, distanceUnits::in);
-  rotate(360);
+  auto3();
 }
 
 void usercontrol( void ) {
-  while (1) {
+  while (true) {
     double LX = Controller1.Axis4.position(pct);
     double LY = Controller1.Axis3.position(pct);
     double RX = Controller1.Axis1.position(pct);
@@ -116,8 +166,6 @@ void usercontrol( void ) {
       C.setStopping(hold);
     }
     
-
-    Controller1.Screen.print(C.rotation(deg));
     task::sleep(20);
   }
 }
